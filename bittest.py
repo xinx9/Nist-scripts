1f349049b4ed6faa5e2643935f3767980f4396ad
Brandon Burke <bcburke93@gmail.com>
2019-04-22 22:14:52 -0500

    more scripts

--- bittest.py
+++ bittest.py
@@ -4,7 +4,7 @@ def readfiles(filename):
     fpe = "/results.txt"
     filepath = fps + filename + fpe
     fileread = open(filepath,"r")
-    fileoutput = file.read()
+    fileoutput = fileread.read()
     fileread.close()
     return fileoutput
 
@@ -12,7 +12,7 @@ def readfiles(filename):
 ##                Output File             ##
 ############################################
 
-out = open("dataOutput.txt","a")
+out = open("output.txt","a")
 
 ############################################
 ##                Input File              ##
@@ -33,38 +33,48 @@ FFT = readfiles("FFT")
 CS = readfiles("CumulativeSums")
 LR = readfiles("LongestRun")
 
-
+out.write("Approximate Entropy:\n")
 out.write(AE)
 out.write("\n\n")
+out.write("NonOverlapping Template:\n")
 out.write(NOT)
 out.write("\n\n")
+out.write("Frequency:\n")
 out.write(FQ)
 out.write("\n\n")
+out.write("Linear Complexity:\n")
 out.write(LC)
 out.write("\n\n")
+out.write("Block Frequency:\n")
 out.write(BFQ)
 out.write("\n\n")
+out.write("Overlapping Template:\n")
 out.write(OLT)
 out.write("\n\n")
+out.write("Rank:\n")
 out.write(RANK)
 out.write("\n\n")
+out.write("Universal:\n")
 out.write(UNI)
 out.write("\n\n")
-out.write(RANK)
-out.write("\n\n")
-out.write(UNI)
-out.write("\n\n")
+out.write("Serial:\n")
 out.write(SERI)
 out.write("\n\n")
+out.write("Runs:\n")
 out.write(RUNS)
 out.write("\n\n")
+out.write("Random Excursions Variant:\n")
 out.write(REV)
 out.write("\n\n")
+out.write("Random Excursions:\n")
 out.write(RE)
 out.write("\n\n")
+out.write("FFT:\n")
 out.write(FFT)
 out.write("\n\n")
+out.write("Cumulative Sumation:\n")
 out.write(CS)
 out.write("\n\n")
+out.write("Longest Run:\n")
 out.write(LR)
 out.write("\n\n")
